3fd4ddeb80a08209b440a232cbf8ba9816cd7347
oakes1218 <oakes1218@hotmail.com>
2015-10-29 16:19:21 +0800

    addjsoncompareisonresult

--- Javawork/src/Jsonpractice/JsonFormRead.java
+++ Javawork/src/Jsonpractice/JsonFormRead.java
@@ -5,20 +5,50 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import org.json.JSONArray;
+import java.util.Scanner;
+
 import org.json.JSONObject;
+import org.json.JSONArray;
+
+import com.sun.org.apache.xalan.internal.xsltc.compiler.Pattern;
+
 
 public class JsonFormRead {
 	
-	public static void main (String ager) throws IOException{
+	private static Scanner scanner;
+
+	public static void main (String[] args) throws IOException{
+		scanner = new Scanner(System.in);
+		StringBuffer sb = new StringBuffer();
 		
+		//Pattern pattern = Pattern.compile(sb); 
 		URL url = new URL ("http://data.taipei.gov.tw/opendata/apply/json/QTdBNEQ5NkQtQkM3MS00QUI2LUJENTctODI0QTM5MkIwMUZE");
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 		BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
         String jsonString = reader.readLine();
         reader.close();
-		//String NAME = JSONObject(jsonString).getString("name");
-        JSONObject jsonObject ager= new JSONObject();
+       
+        JSONArray jsonArray = new JSONArray(jsonString);
+        JSONObject jsonObject = new JSONObject();
+       
+        while (true) {
+        	
+            System.out.println("請輸入飯店名稱：");
+         	scanner.next();
+         	
+        	for (int i = 0 ; i < jsonArray.length() ; i++ ){
+            	jsonObject = jsonArray.getJSONObject(i);   
+            	System.out.println(jsonObject);
+            	sb.append(jsonObject);
+            	if(scanner.next().equals(jsonObject)){
+            		System.out.println(jsonString);
+            		break;
+            	}
+            }
+			
+		}
+
+        
 	}
 
 }
